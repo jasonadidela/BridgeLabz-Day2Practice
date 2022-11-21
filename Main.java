10e457ee5ff87951d574ed57b548176db416b48f
jasonadidela <jasonadidela@gmail.com>
2022-11-22 00:24:45 +0530

    Created 2.11_QuadraticEquation final code.

--- Main.java
+++ Main.java
@@ -1,6 +1,33 @@
+//3. Write a program Quadratic.java to find the roots of the equation a*x*x + b*x + c.
+//Since the equation is x*x, hence there are 2 roots. The 2 roots of the equation can be found using a formula
+//delta = b*b - 4*a*c
+//Root 1 of x = (-b + sqrt(delta))/(2*a)
+//Root 2 of x = (-b - sqrt(delta))/(2*a)
+//Take a, b, and c as input values to find the roots of x.
+
+import  java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to BridgeLabz-Day2Practice repo");
-        System.out.println("Welcome to 2.11_QuadraticEquation Branch");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the value of a: ");
+        double a = input.nextDouble();
+        System.out.print("Enter the value of b: ");
+        double b = input.nextDouble();
+        System.out.print("Enter the value of c: ");
+        double c = input.nextDouble();
+        double d= b * b - 4.0 * a * c;
+        if (d> 0.0){
+                double r1 = (-b + Math.pow(d, 0.5)) / (2.0 * a);
+                double r2 = (-b - Math.pow(d, 0.5)) / (2.0 * a);
+                System.out.println("The roots are " + r1 + " and " + r2);
+            }
+        else if (d == 0.0){
+                double r1 = -b / (2.0 * a);
+                System.out.println("The root is " + r1);
+            }
+            else{
+                System.out.println("Roots are not real.");
+            }
     }
 }
