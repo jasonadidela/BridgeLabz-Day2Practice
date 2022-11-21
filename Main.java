b722cf2cc4539172decb484cb70a1f29e0af13a2
jasonadidela <jasonadidela@gmail.com>
2022-11-22 00:56:03 +0530

    Created 2.6_ReverseNumFor final code.

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
+//Programs With For Loop
+//2. Write a Program to reverse the integer number eg. Input n=231 reverse is 132
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to BridgeLabz-Day2Practice repo");
-        System.out.println("Welcome to 2.6_ReverseNumFor Branch");
+        Scanner n=new Scanner(System.in);
+        System.out.println("Enter The number:");
+        int number = n.nextInt();
+        int reverse = 0;
+        for( ;number != 0; number=number/10){
+                int remainder = number % 10;
+                reverse = reverse * 10 + remainder;
+            }
+            System.out.println("The reverse of the given number is: " + reverse);
     }
 }
