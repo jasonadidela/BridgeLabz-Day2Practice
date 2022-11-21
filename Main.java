7764ef726d99696e65f2f6f73d132d52c1d81bf2
jasonadidela <jasonadidela@gmail.com>
2022-11-22 00:36:51 +0530

    Created 2.9_MonthNumberSwitch code.

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
+//2. Write a program SpringSeason.java that takes two int values m and d from the command line and prints true if day d of
+// month m is between March 20 (m = 3, d=20) and June 20 (m = 6, d = 20), false otherwise.
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to BridgeLabz-Day2Practice repo");
