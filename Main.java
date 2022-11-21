eca4e44d5637ca135e33a4981f7ea5aa93efac06
jasonadidela <jasonadidela@gmail.com>
2022-11-22 00:58:04 +0530

    Created 2.5_SumNumFor final code.

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
+//Programs With For Loop
+//1. Write a Program for the sum of n natural numbers eg. Input N=5 output 1+2+3+4+5
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to BridgeLabz-Day2Practice repo");
-        System.out.println("Welcome to 2.5_SumNumFor Branch");
+        Scanner n=new Scanner(System.in);
+        System.out.println("Enter The number:");
+        int in = n.nextInt();
+        int i, sum = 0;
+        for(i = 1; i <= in; ++i){
+                sum = sum + i;
+            }
+            System.out.println("Sum of First 10 Natural Numbers is = " + sum);
     }
 }
