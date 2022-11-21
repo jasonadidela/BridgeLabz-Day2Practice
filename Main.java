c167d678bfb2ae9b69b8afc1612efc7127999eb7
jasonadidela <jasonadidela@gmail.com>
2022-11-22 01:02:44 +0530

    Created 2.3_SumNumWhile final code.

--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
+//Programs With While Loop
+//1. Write a Program for the sum of n natural numbers eg. Input N=5 output 1+2+3+4+5
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to BridgeLabz-Day2Practice repo");
-        System.out.println("Welcome to 2.3_SumNumWhile Branch");
+        int x, i = 1;
+        int sum = 0;
+        System.out.println("Enter Number of items :");
+        Scanner scanner = new Scanner(System.in);
+        x = scanner.nextInt();
+        while(i <= x){
+                sum = sum +i;
+                i++;
+            }
+        System.out.println("Sum of "+x+" numbers is :"+sum);
     }
 }
